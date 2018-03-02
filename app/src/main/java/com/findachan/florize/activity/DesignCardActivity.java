88fa815dd22450b22390a3ede60baf0c6a2c93c6
reiva5 <jehiannormansaviero@gmail.com>
2018-03-02 11:11:57 +0700

    Re-edit DesignCardActivity to pass Intent

--- app/src/main/java/com/findachan/florize/activity/DesignCardActivity.java
+++ app/src/main/java/com/findachan/florize/activity/DesignCardActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.findachan.florize.R;
 
 public class DesignCardActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private Intent intentPrevious;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +22,30 @@ public class DesignCardActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        intentPrevious = getIntent();
 
         final Button button = findViewById(R.id.button_wishes);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(DesignCardActivity.this, AndroidLoadImageFromURLActivity.class);
+                Intent myIntent = new Intent(DesignCardActivity.this, PaymentActivity.class);
+                myIntent.putExtra("id", intentPrevious.getStringExtra("id"));
+                myIntent.putExtra("price", intentPrevious.getStringExtra("price"));
+                myIntent.putExtra("name", intentPrevious.getStringExtra("name"));
+                myIntent.putExtra("url", intentPrevious.getStringExtra("url"));
                 startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed(){
+        intentPrevious = getIntent();
+        Intent myIntent = new Intent();
+        myIntent.putExtra("id", intentPrevious.getStringExtra("id"));
+        myIntent.putExtra("price", intentPrevious.getStringExtra("price"));
+        myIntent.putExtra("name", intentPrevious.getStringExtra("name"));
+        myIntent.putExtra("url", intentPrevious.getStringExtra("url"));
+        setResult(AppCompatActivity.RESULT_OK, myIntent);
+        super.onBackPressed();
     }
 }
