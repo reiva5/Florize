62d3f8dbcee08b3b7c9e28d6c49db79f1a5d3095
findafn <findafn@gmail.com>
2018-03-02 14:59:14 +0700

    fix bug

--- app/src/main/java/com/findachan/florize/activity/MyFirebaseMessagingService.java
+++ app/src/main/java/com/findachan/florize/activity/MyFirebaseMessagingService.java
@@ -1,5 +1,13 @@
 package com.findachan.florize.activity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -7,6 +15,8 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
+    private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
@@ -15,5 +25,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated.
         Log.w(TAG, "From: " + remoteMessage.getFrom());
         Log.w(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel("4655", "not", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotifyManager =(NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this,"4655")
+                .setContentTitle("FLORIZE")
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setContentIntent(notificationPendingIntent)
+                .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                .setDefaults(Notification.DEFAULT_ALL);
+        Notification myNotification = notifyBuilder.build();
+        mNotifyManager.notify((int)(System.currentTimeMillis()/1000), myNotification);
     }
 }
